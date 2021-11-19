bdf868470df90ecae987a87a8ea4db25f6339e01
santhoshkumar-27 <it.santhoshkumar@outlook.com>
2021-11-19 18:33:40 +0530

    till reactive form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'reactiveform';
+export class AppComponent implements OnInit {
+  loginPage: FormGroup;
+  submitted = false;
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+  this.loginPage = this.formBuilder.group({
+    username : ['', Validators.required],
+    password : ['', [Validators.required, Validators.email]]
+  });
+  }
+ 
+
+  clicksub() {
+    this.submitted = true;
+    console.log(this.loginPage.value);
+  }
+  get f() { return this.loginPage.controls; }
+
+
+
+
+
 }
